a944b061a2ec393f00913fc74a5654e23bf558e4
isun314058-cyber <isun314058@gmail.com>
2026-05-29 15:08:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1709,7 +1709,7 @@ if mode == "🆕 新建預定進度表":
                             pile_x = x - (pile_width // 2)
                             
                             # 自動依字體大小調整高度
-                            pile_y = y - r - pile_height - 6
+                            pile_y = y - r - pile_height - 8
                             
                             draw.text(
                                 (
