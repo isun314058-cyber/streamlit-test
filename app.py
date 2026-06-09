6d7086e6ad525e791c12b9ef73cc00685fae5f7a
isun314058-cyber <isun314058@gmail.com>
2026-06-09 16:40:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1146,11 +1146,11 @@ def create_schedule(
         
             break
 
-    #result = optimize_tail_days(
-        #result,
-        #neighbor_map,
-        #daily_count
-    #)
+    result = optimize_tail_days(
+        result,
+        neighbor_map,
+        daily_count
+    )
 
     return result
 
