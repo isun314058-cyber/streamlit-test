53a81d9cbef4fd7a969496196f10e93f5cfaf58e
isun314058-cyber <isun314058@gmail.com>
2026-06-10 17:42:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1723,7 +1723,7 @@ if mode == "新建預定進度表":
                             progress_bar.progress(percent)
                         
                             progress_text.info(
-                                f"🤖 AI正在分析最佳施工排程中，請稍候...\n\nAI分析進度：{percent}% "
+                                f"🤖 AI正在分析最佳施工排程中，請稍候...\n\nAI分析進度：{percent}%"
                             )
                                                        
                             schedule = create_schedule(
