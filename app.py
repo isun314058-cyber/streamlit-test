e3cfda75695c89fee51e1b9dc63365ec9921db60
isun314058-cyber <isun314058@gmail.com>
2026-06-11 08:19:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2935,7 +2935,15 @@ elif mode == "修正當前進度表":
                         .fillna("")
                         .astype(str)
                     )
-        
+
+                    st.write("原始Excel顏色")
+                    
+                    st.dataframe(
+                        original_df[
+                            ["施工日","日期顏色"]
+                        ]
+                    )
+
                     required_cols = [
                         "施工日",
                         "日期",
@@ -2961,9 +2969,6 @@ elif mode == "修正當前進度表":
                         st.markdown("---")
                         st.write("✏️ 請於下方修改施工樁號")
                         
-                        editor_df = original_df.drop(
-                            columns=["日期顏色"]
-                        )
 
                         editor_df["施工數量"] = (
                             editor_df["施工數量"]
@@ -3404,6 +3409,14 @@ elif mode == "修正當前進度表":
                                 
                                 new_df = original_df.copy()
 
+                                st.write("續排前")
+                                
+                                st.dataframe(
+                                    new_df[
+                                        ["施工日","日期顏色"]
+                                    ]
+                                )
+
                                 new_df["施工樁號"] = edit_df["施工樁號"]
                                 new_df["施工數量"] = edit_df["施工數量"]
                                 
@@ -3425,7 +3438,15 @@ elif mode == "修正當前進度表":
                                 
                                     new_df.at[target_row, "施工樁號"] = pile_text
 
-                                    new_df.at[target_row, "日期顏色"] = day_data["日期顏色"]
+                                    old_color = str(
+                                        new_df.at[target_row,"日期顏色"]
+                                    ).strip()
+                                    
+                                    if old_color == "":
+                                        new_df.at[
+                                            target_row,
+                                            "日期顏色"
+                                        ] = day_data["日期顏色"]
                                 
                                     new_df.at[target_row, "施工數量"] = str(
                                         len(day_data["施工樁號"])
